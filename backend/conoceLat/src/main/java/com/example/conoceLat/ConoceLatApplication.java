dc392ee813adb57561015830a02c330c1788a954
mafegarciap <mafegarciap@gmail.com>
2023-03-29 12:49:37 -0300

    Crear proyecto en Spring Boot y conexión con la base de datos local

---
+++ backend/conoceLat/src/main/java/com/example/conoceLat/ConoceLatApplication.java
@@ -0,0 +1,13 @@
+package com.example.conoceLat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConoceLatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConoceLatApplication.class, args);
+	}
+
+}
---
+++ backend/conoceLat/src/test/java/com/example/conoceLat/ConoceLatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conoceLat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConoceLatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
