e5291eb2275b251d795899709ba61f5d819e05eb
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-04-12 13:44:06 +0000

    Pending changes exported from your codespace

--- backend/conocemelat/src/main/java/com/example/conocemelat/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.conocemelat.repository;
-
-import com.example.conocemelat.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends JpaRepository<City, Long> {
-}
