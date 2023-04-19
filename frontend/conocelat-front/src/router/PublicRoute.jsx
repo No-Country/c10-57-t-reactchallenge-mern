c93858aea318ac09d984179a330a5e46630510ac
ccama22 <ccamacruz2018@gmail.com>
2023-04-19 15:49:30 -0500

    login and register with backends

--- frontend/conocelat-front/src/router/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Children } from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-export const PublicRoute = ({Children}) => {
-  const dataUser = useSelector((state) => state.auth);
-  console.log("ccama data user",dataUser.status==="not-authenticated");
-  return dataUser.status==="not-authenticated" ? Children: <Navigate to="/"/>;
-}
