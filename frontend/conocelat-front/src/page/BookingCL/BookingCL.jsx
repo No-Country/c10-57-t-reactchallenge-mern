85eac1a5e01b85824b8af96b961931543d40d834
ccama22 <ccamacruz2018@gmail.com>
2023-04-19 18:23:26 -0500

    rol of type user

---
+++ frontend/conocelat-front/src/page/BookingCL/BookingCL.jsx
@@ -0,0 +1,11 @@
+import "./BookingCL.css";
+
+export const BookingCL = () => {
+  return (
+    <div className="container-booking">
+      <div className='booking-data'>
+
+      </div>
+    </div>
+  )
+}
